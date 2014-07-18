f73e67c46a5b758d5ba511181a9953d219282c5c
Connor Smith <cmds4410@gmail.com>
2014-07-18 16:11:35 -0700

    Initial Commit

---
+++ CoreAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreAnimation
+//
+//  Created by Connor Smith on 7/18/14.
+//  Copyright (c) 2014 Connor Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
