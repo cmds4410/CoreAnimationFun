da482176e3e647f869b1736d81493df5c2bcf4d7
Connor Smith <cmds4410@gmail.com>
2014-07-29 15:12:54 -0400

    removed unused methods

--- CoreAnimationDemo Tests/CoreAnimationDemo_Tests.swift
+++ CoreAnimationDemo Tests/CoreAnimationDemo_Tests.swift
@@ -14,21 +14,6 @@ class CoreAnimationDemo_Tests: XCTestCase, CACircleViewDelegate {
     let testVC = UIViewController()
     var animationExpectation:XCTestExpectation?
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
     func testAnimationCompletion() {
         let testView = CACircleView()
         testView.animationDelegate = self
